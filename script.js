79d0d7bc14d9c95dc7e18966c5d55dcbf1152299
Lorenzo <lorenzozabot@gmail.com>
2022-07-10 16:43:19 +0200

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+function operate(operator , a, b){
+    switch(operator){
+        case "+": add(a,b);
+                    break;
+        case "-": subtract(a,b);
+                    break;
+        case "*": multiply(a,b);
+                    break;
+        case "/": divide(a,b);
+                    break;
+    }
+}
