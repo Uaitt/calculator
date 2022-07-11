f348fd7011b0f379f8ad6839f39a396817f9717d
Lorenzo <lorenzozabot@gmail.com>
2022-07-11 14:53:25 +0200

    Create calculator mechanism

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function operate(operator, a, b){
     }
 }
 
-//get the references
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");   //remember to remove ac and clear
+//get the references 
+const numbers = document.querySelectorAll(".number");       
+const operators = document.querySelectorAll(".operator"); 
 const upperScreen = document.querySelector("#upper");
 const lowerScreen = document.querySelector("#lower");
 
@@ -34,38 +34,63 @@ let firstOperator = null;
 let secondNumber = null;
 let secondOperator = null;
 
-let str = "";
+let numbersString = "";
 
-//add the event listeners for the buttons
+//add the event listeners for the numbers
 numbers.forEach( number => number.addEventListener( "click", () =>{
     upperScreen.textContent += number.textContent;
 
-    str += number.textContent;
-}))
+    numbersString += number.textContent;    //append the current selected number in the string
+}));
 
+//add the event listeners for the operators
 operators.forEach( operator => operator.addEventListener( "click", () =>{
     
-    //if this is the first operator
+    //if this is the first operator being selected 
     if (firstOperator === null){
-        firstNumber = +str;
-        str="";
+        firstNumber = +numbersString;
+        numbersString="";
         firstOperator = operator.getAttribute("id");      
     }
     else{
-        secondNumber = +str;
-        str="";
+        if(secondOperator === "="){}
+        else{        
+        secondNumber = +numbersString;
+        numbersString="";
+        }
         secondOperator = operator.getAttribute("id");
     }
 
-    //if the operator is an equal
-    if (operator.getAttribute("id") === "="){
-        console.log(firstNumber,firstOperator,secondNumber,secondOperator)
+    if (operator.getAttribute("id") === "=") {
+        
+        //if the second number does not exist do nothing
+        if (secondNumber === null){
+            
+        }
+        else{              
+            lowerScreen.textContent = operate(firstOperator,firstNumber,secondNumber);
+
+            firstNumber = operate(firstOperator,firstNumber,secondNumber);
+            
+            //at this point you can do nothing and just wait for the next operator to be clicked
+        }
+    }
+    //if the two operators are both assigned, this means that the expression has to be evaluated but no = was provided
+    else if (firstOperator !== null && secondOperator !== null){
         lowerScreen.textContent = operate(firstOperator,firstNumber,secondNumber);
+
+        if(operator.getAttribute("id") === "!" || operator.getAttribute("id") === "^")
+            upperScreen.textContent +=" " + operator.getAttribute("id")+ " "; 
+        else
+            upperScreen.textContent +=" " + operator.textContent +" ";
+
+        firstNumber = operate(firstOperator,firstNumber,secondNumber);
+        firstOperator = secondOperator; //the operator that was just selected will be the one passed to the operate function
     }
     else if(operator.getAttribute("id") === "!" || operator.getAttribute("id") === "^")
-        upperScreen.textContent += operator.getAttribute("id");
-    else
-        upperScreen.textContent += operator.textContent;
+        upperScreen.textContent += " " + operator.getAttribute("id") +  " "; 
+    else 
+        upperScreen.textContent += " " + operator.textContent + " ";
 
-}))
+}));
 
